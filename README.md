40bf8ff8952e815d4566dda32b463534e398419d
Luca Mattiazzi <luca.mattiazzi@illimity.com>
2024-07-15 16:09:04 +0200

    add claude

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Top Hat
+# Magic Top Hat
+
+> ur: la cosa producida por sugestión, el objeto educido por la esperanza
+>
+> -- <cite>Jorge Luis Borges - Tlön, Uqbar, Orbis Tertius</cite>
+
+**This is a joke library, it will evaluate and run potentially harmful code: use it as much as possible**
 
 Do you know how magicians could pull stuff out of an apparently empty top hat?
 
-I only recently discovered how that worked, and apparently it was dunder methods all along! Who would have thought!
+I only recently discovered how that worked, and apparently it was dunder methods and llms all along! Who would have thought!
 
 So I decided to implement something similar in Python: you can import any method whatsoever from this magical `magic_top_hat`, even if the module itself seems empty!
 
@@ -30,15 +36,27 @@ def get_today_day() -> str:
 
 and a function will be generated that satisfies your every need*!
 
+###### * satisfaction not guaranteed. You'll need access to an llm by setting either OPENAI_API_KEY or CLAUDE_API_KEY
+
 Do you need to know how the function generated your result? No, right? Implementation is just a detail for the code monkeys! If you really have no trust in this (as you should), you can set the environmental variable `I_DONT_TRUST_ROBOTS` to have each implementation printed out.
 
-*satisfaction not guaranteed. You will need to set a `OPENAI_API_KEY` env parameter for this to work. And even then, it might not work.
+## How it works
 
-## How
+Well, I simply send the caller code to an llm and ask to generate a function that will fulfill the request, given the context in which it was called, the name it was given and the parameters it received.
 
-Well, I simply send the caller code to OpenAI and ask ChatGPT to generate a function that will fulfill the request, given the context in which it was called, the name it was given and the parameters it received.
+So the results may vary from abysmal to grotesque.
 
-So the results may vary from abysmal to groteque.
+## How to use it
+
+```python
+from magic_top_hat import get_day_out_of_datetime
+from datetime import datetime
+
+def get_today_day() -> str: 
+    today = datetime.now()
+    day = get_day_out_of_datetime(today)
+    return day # this will return the day name
+```
 
 ## Why
 
--- src/magic_top_hat/__init__.py
+++ src/magic_top_hat/__init__.py
@@ -4,8 +4,7 @@ import pprint
 import traceback
 from functools import partial
 from importlib.util import module_from_spec, spec_from_file_location
-
-from openai import OpenAI
+from random import choice
 
 __all__ = []
 
@@ -17,29 +16,60 @@ def __dir__():
 def __getattr__(name):
     if name == "__path__":
         return None
-    if name in ["__ask", "__magic"]:
-        raise AttributeError("Really?? You named your function that? Come on!")
+    if name.startswith("__"):
+        raise AttributeError("Come one two underscores are off limits!")
     return partial(__magic, name)
 
 
-def __ask(fn_name: str, caller_code: str) -> str:
-    client = OpenAI(
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
+def __get_messages(fn_name: str, caller_code: str) -> list[dict[str, str]]:
+    return [
+        {
+            "role": "system",
+            "content": "You are a software engineer. You will receive a snippet of code, and must write implement a single function so that the snippet works correctly. The code is Python 3, and your answer must only contain the function body, not the entire program. You can assume the function will be called with valid arguments. The name of the function is provided in the prompt.",
+        },
+        {
+            "role": "user",
+            "content": f"You must implement the function called {fn_name} that makes the following snippet work correctly. {caller_code}",
+        },
+    ]
+
+
+def __ask_openai(fn_name: str, caller_code: str) -> str:
+    from openai import OpenAI
+
+    client = OpenAI()
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "system",
-                "content": "You are a software engineer. You will receive a snippet of code, and must write implement a single function so that the snippet works correctly. The code is Python 3, and your answer must only contain the function body, not the entire program. You can assume the function will be called with valid arguments. The name of the function is provided in the prompt.",
-            },
-            {
-                "role": "user",
-                "content": f"You must implement the function called {fn_name} that makes the following snippet work correctly. {caller_code}",
-            },
-        ],
-        model="gpt-3.5-turbo",
+        messages=__get_messages(fn_name, caller_code),
+        model="gpt-4o",
     )
     return chat_completion.choices[0].message.content
+
+
+def __ask_claude(fn_name: str, caller_code: str) -> str:
+    from anthropic import Anthropic
+
+    client = Anthropic()
+    chat_completion = client.messages.create(
+        messages=__get_messages(fn_name, caller_code),
+        model="claude-3-5-sonnet-20240620",
+    )
+    return chat_completion.choices[0].message.content
+
+
+def __ask(fn_name: str, caller_code: str) -> str:
+    openai_available = os.getenv("OPENAI_API_KEY") is not None
+    anthropic_available = os.getenv("ANTHROPIC_API_KEY") is not None
+
+    if not openai_available and not anthropic_available:
+        raise ValueError(
+            "Neither OPENAI_API_KEY nor ANTHROPIC_API_KEY is set. Write your own code!."
+        )
+    if openai_available and not anthropic_available:
+        return __ask_openai(fn_name, caller_code)
+    if anthropic_available and not openai_available:
+        return __ask_claude(fn_name, caller_code)
+
+    return choice([__ask_claude, __ask_openai])(fn_name, caller_code)
 
 
 def __magic(fn_name: str, *args, **kwargs):
@@ -52,10 +82,8 @@ def __magic(fn_name: str, *args, **kwargs):
     raw_python = __ask(fn_name, caller_code)
     if os.environ.get("I_DONT_TRUST_ROBOTS"):
         pprint.pprint(raw_python)
-    fn_definition = (
-        raw_python.split("python\n")[1]
-        .replace("```", "")
-        .replace(fn_name, f"{fn_name}_implementation")
+    fn_definition = raw_python.replace("python\n```", "").replace(
+        fn_name, f"{fn_name}_implementation"
     )
     exec(fn_definition, globals())
     fn = globals()[f"{fn_name}_implementation"]
